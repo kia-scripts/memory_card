950185a31373d4e83d693ed2a2cc51e82dd4ec67
Ilia Koptiaev <ilya_koptyaev@mail.ru>
2026-02-27 02:21:13 +0300

    second begin

--- memory.py
+++ memory.py
@@ -2,5 +2,7 @@ answer = input('что нужно?')
 while answer != 'off':
     if answer == '1':
         print('ого')
+    elif answer == '2':
+        print('ну да')
     answer = input('что нужно?')
     
